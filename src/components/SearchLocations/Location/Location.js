ad39217d83b46111154591e5abb25a8bde2ac1de
AgonIdrizi <agon.88@hotmail.com>
2019-08-14 18:31:35 +0200

    add trimLocationName function

--- src/components/SearchLocations/Location/Location.js
+++ src/components/SearchLocations/Location/Location.js
@@ -24,20 +24,21 @@ const Location = (props) => {
     setStyleFavIcon({color: 'blue'})
   }
 
+  const trimLocationName=(str) => {
+    return str.length>32 ? str.substring(0,32).concat('...') : str
+  }
+
  
   let addRemoveFavHandlers = styleFavIcon.color == 'blue' ? (e) => add(e) : e => remove(e)
 
-  // let favIcon = props.loggedIn ? (<a href="#" onClick= {addRemoveFavHandlers}>
-  //                                     <span  ><FontAwesomeIcon style={styleFavIcon} icon={faStar} /></span>
-  //                                   </a>) :
-  //                                   null
+  
   const favIcon = (props.loggedIn && props.isFavourite ) ? (<a href="#" onClick= {(e) => remove(e)}>
                                                               <span  ><FontAwesomeIcon style={{color: 'yellow'}} icon={faStar} /></span>
                                                             </a>) 
                                                           : (props.loggedIn) ?(<a href="#" onClick= {addRemoveFavHandlers}>
                                                                                 <span  ><FontAwesomeIcon style={styleFavIcon} icon={faStar} /></span>
                                                                                </a>) 
-                                                                            : null
+                                                                              : null
   return (
         <div  className={classes.Location} >
             <div className={classes.LocationImageInfo}
@@ -46,8 +47,8 @@ const Location = (props) => {
               //here we close the opening sidemenu and update selectedLocation
               onClick={e => props.onSelectLocation(props.longitude, props.latitude,props.place,e, props.history)}>
               <img src='https://www.meteoblue.com/website/images/flags/mk.svg' style={{height: '15px', width: '15px'}} />
-              <p >{props.place}</p>
-              <span>1314m, {props.longitude} N {props.latitude}</span>
+              <p >{trimLocationName(props.place)}</p>
+              <span> {props.longitude.toFixed(2)} N {props.latitude.toFixed(2)} E</span>
             </div>
             <div >
               {favIcon}

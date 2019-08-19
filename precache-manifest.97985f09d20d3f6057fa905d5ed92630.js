249a98d5352029db692a900c61d3ec37bbb27766
AgonIdrizi <agon.88@hotmail.com>
2019-08-19 19:04:13 +0200

    Updates

---
+++ precache-manifest.97985f09d20d3f6057fa905d5ed92630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7154e8212e0e7bf706d31c47914d8ee0",
+    "url": "/meteoup/index.html"
+  },
+  {
+    "revision": "d40fc3cd0159d85c1778",
+    "url": "/meteoup/static/css/2.d887cd30.chunk.css"
+  },
+  {
+    "revision": "a11fc46200c9b2442620",
+    "url": "/meteoup/static/css/main.bc51da4f.chunk.css"
+  },
+  {
+    "revision": "d40fc3cd0159d85c1778",
+    "url": "/meteoup/static/js/2.ea7f512a.chunk.js"
+  },
+  {
+    "revision": "a11fc46200c9b2442620",
+    "url": "/meteoup/static/js/main.77ead024.chunk.js"
+  },
+  {
+    "revision": "12e1880d9f8ba6ecbaa3",
+    "url": "/meteoup/static/js/runtime~main.7a249665.js"
+  }
+]);
